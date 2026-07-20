90fc74d7baf899777c26d86f7fd5830d27eb1651
Gioworknow <innovaeducpro@gmail.com>
2026-07-20 00:13:10 -0300

    Remove configuração local do controle de versão

--- api/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'db_host' => 'localhost',
-    'db_name' => 'u594323539_IUt1u',
-    'db_user' => 'u594323539_MN2ZE',
-    'db_pass' => 'D54sbCnRKDX98w#',
-    'app_url' => 'https://innovaenvios.app',
-];
